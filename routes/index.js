0f254a4000cd9f1e868a31e2c45436d759827e65
Ankit Shingavi <ankit.shingavi@mavs.uta.edu>
2016-11-29 16:48:28 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var database;
 
 
 router.get('/', function(req, res, next) {
-	//MongoClient.connect("mongodb://localhost:27017/mydb", function(err, db) {
 	MongoClient.connect("mongodb://ankit:ankit@ds113678.mlab.com:13678/heroku_1kt0jn7x", function(err, db) {
 		if(!err) {
 			database = db;
@@ -20,19 +19,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/data', function(req, res, next) {
-	console.log("in get method");
 	var collection = database.collection('Nobel');
 	var id = parseInt(req.query.id);
-	console.log(typeof(id));
-	console.log(id);
 	collection.findOne({Id: id},function(err, doc) {
 		if (doc){			
-			console.log(doc);
-			console.log(typeof(doc));
 			res.json(doc)
 		} 
 		else {
-			console.log('no data for this company');
+			console.log('no data found');
 		}
 	});
 });
